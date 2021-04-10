a83bf0f64527f7544c1202a1d6ae6648c1de0ee0
MrRobot420 <max.karlchen@web.de>
2021-04-10 14:20:45 +0200

    use sprite instead of image in order to make the background transparent

--- test/mouse_coordinates.py
+++ test/mouse_coordinates.py
@@ -10,8 +10,10 @@ class MainWindow(pyglet.window.Window):
         super().__init__(*args, **kwrgs)
         pyglet.gl.glClearColor(0.9,0.9,0.9,1)
         self.image = pyglet.image.load('./resources/cursor/red_cross.png')
-        self.image_width = 64
-        self.image_height = 64
+        self.background = pyglet.graphics.Batch()
+        self.cursor = pyglet.sprite.Sprite(self.image, 0, 0)
+        self.image_width = self.image.width
+        self.image_height = self.image.height
 
         self.x, self.y = 0, 0
 
@@ -32,7 +34,6 @@ class MainWindow(pyglet.window.Window):
     def on_mouse_motion(self, x, y, dx, dy):
         self.mouse_x = x
         self.mouse_y = y
-        # self.image.blit(x, y, z=0, width=self.image_width, height=self.image_height)
 
     def on_key_release(self, symbol, modifiers):
         try:
@@ -48,25 +49,23 @@ class MainWindow(pyglet.window.Window):
 
     def render(self):
         self.clear()
-
-        ## Add stuff you want to render here.
-        ## Preferably in the form of a batch.
-        self.image.blit(self.mouse_x-self.image_width/2, self.mouse_y-self.image_height/2, z=0, width=self.image_width, height=self.image_height)
+        scale = 0.07
+        width = (self.image_width * scale) / 2
+        height = (self.image_height * scale) / 2
+        self.cursor = pyglet.sprite.Sprite(self.image, self.mouse_x-width, self.mouse_y-height, batch=self.background)
+        self.cursor.scale = scale
+        self.cursor.draw()
 
         self.flip()
     
     def run(self):
         while self.alive == 1:
             self.render()
-
-            # -----------> This is key <----------
-            # This is what replaces pyglet.app.run()
-            # but is required for the GUI to not freeze
-            #
             event = self.dispatch_events()
 
     
 if __name__ == '__main__':
     # pyglet.clock.schedule_interval(update, 1 / 120.0)
     game_window = MainWindow(1280, 960, 'Game Name', resizable=True)
+    game_window.set_mouse_visible(False) # Hide the mouse cursor
     game_window.run()
