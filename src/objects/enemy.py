419d4d0b084093b1550f7c7857f78bb0d67ef0c2
Johannes Velde <jo.velde@gmx.de>
2021-04-10 21:34:01 +0200

    Enemy added TODO render enemies

---
+++ src/objects/enemy.py
@@ -0,0 +1,21 @@
+from pyglet import shapes, clock
+
+
+class Enemy:
+    def __init__(self, speed, health, size, x_pos, y_pos, background):
+        self.speed = speed
+        self.health = health
+        self.size = size
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.background = background
+        self.clock = clock
+
+    def draw(self, x, y):
+        square = shapes.Rectangle(x, y, self.size, self.size, color=(55, 55, 2), batch=self.background)
+        square.draw()
+        pass
+
+    def update(self):
+        print(f"Enemy last update: {self.clock.tick()}")
+        self.x_pos -= self.speed * self.clock.tick()
--- src/rain_man.py
+++ src/rain_man.py
@@ -1,23 +1,25 @@
 import pyglet
+from pyglet import clock
 from pyglet.window import mouse
 from pyglet.gl import *
-
+import random
 from gui.cursor import Cursor as Cursor
 from gui.label import Label as Label
+from objects.enemy import Enemy
 
 key = pyglet.window.key
 
 
 class MainWindow(pyglet.window.Window):
     def __init__(self, *args, **kwrgs):
-        super().__init__(*args, **kwrgs)
+        super().__init__(*args, **kwrgs, vsync=False)
         pyglet.gl.glClearColor(0.9, 0.9, 0.9, 1)
         self.background = pyglet.graphics.Batch()
         self.cursor = Cursor(self.background)
-
+        self.clock = clock
         self.x, self.y = 0, 0
         self.cursor_info = Label(f'x: {self.x}, y: {self.y}', self.width - 50, self.height - 36)
-
+        self.enemy = Enemy(100, 100, 30, random.randint(1, self.width), self.height, self.background)
         self.keys = {}
 
         self.mouse_x = 0
@@ -52,17 +54,22 @@ class MainWindow(pyglet.window.Window):
         self.cursor.draw(self.mouse_x, self.mouse_y)
         self.cursor_info = Label(f'x: {self.mouse_x}, y: {self.mouse_y}', self.width - 310, self.height - 36)
         self.cursor_info.draw()
+        self.enemy.draw(250, 5)
 
         self.flip()
 
     def run(self):
         while self.alive == 1:
             self.render()
+            print(f"IN RUN")
             event = self.dispatch_events()
+            self.update()
+
+    def update(self):
+        self.enemy.update()
 
 
 if __name__ == '__main__':
-    # pyglet.clock.schedule_interval(update, 1 / 120.0)
     game_window = MainWindow(1280, 960, 'Game Name', resizable=True)
-    game_window.set_mouse_visible(True)  # Hide the mouse cursor
+    game_window.set_mouse_visible(False)  # Hide the mouse cursor
     game_window.run()
