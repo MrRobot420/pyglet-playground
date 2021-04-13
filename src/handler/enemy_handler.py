2de34c4af11c5bc866a400274af36be5374d43ea
MrRobot420 <max.karlchen@web.de>
2021-04-13 22:36:54 +0200

    add hitboxes

--- src/handler/enemy_handler.py
+++ src/handler/enemy_handler.py
@@ -1,5 +1,6 @@
 import random
 from objects.enemy import Enemy
+from objects.hitbox import Hitbox
 
 class EnemyHandler():
     def __init__(self, screen_width, screen_height, background):
@@ -7,6 +8,8 @@ class EnemyHandler():
         self.screen_height = screen_height
         self.background = background
         self.enemies = []
+        self.hitboxes = []
+        self.inner_hitboxes = []
         self.mouse_x = 0
         self.mouse_y = 0
         self.score = 0
@@ -18,32 +21,39 @@ class EnemyHandler():
         self.mouse_y = mouse_y
         self.score = score
         for index, enemy in enumerate(self.enemies):
+            hitbox = self.hitboxes[index]
+            inner_hitbox = self.inner_hitboxes[index]
+            self.adjust_hitbox_position(enemy, hitbox, index, -5)
+            self.adjust_hitbox_position(enemy, inner_hitbox, index, 0)
             self.adjust_enemy_position(enemy, index, dt)
             self.check_for_collisions(enemy, index)
 
 
-    def generate_enemies(self, amount=10):
+    def generate_enemies(self, amount=100):
         for index in range(amount):
-            self.enemies.append(Enemy(random.randint(1, 10),
-                                      100,
-                                      random.randint(1, self.screen_width),
-                                      self.screen_height,
-                                      self.background))
+            enemySpeed = random.randint(1, 100)
+            x = random.randint(1, self.screen_width)
+            y = self.screen_height
+            newEnemy = Enemy(enemySpeed, 100, x, y, self.background)
+            self.hitboxes.append(Hitbox(enemySpeed, newEnemy.image_width + 10, newEnemy.image_height + 10, x - 5, y - 5, self.background, (255, 55, 55)))
+            self.inner_hitboxes.append(Hitbox(enemySpeed, newEnemy.image_width, newEnemy.image_height, x, y, self.background, (240, 240, 240)))
+            self.enemies.append(newEnemy)
 
 
     def adjust_enemy_position(self, enemy, index, dt):
         if enemy.y_pos <= 0:
             self.enemies.pop(index)
-            self.enemies.append(
-                Enemy(random.randint(1, 10),
-                        100,
-                        random.randint(1, self.screen_width),
-                        self.screen_height,
-                        self.background)
-            )
         else:
             enemy.draw(enemy.x_pos, enemy.y_pos)
             enemy.update(dt)
+
+    
+    def adjust_hitbox_position(self, enemy, hitbox, index, diff):
+        if hitbox.y_pos <= 0:
+            self.hitboxes.pop(index)
+        else:
+            hitbox.draw(enemy.x_pos + diff, enemy.y_pos + diff)
+            
 
 
     def check_for_collisions(self, enemy, index):
--- src/objects/hitbox.py
+++ src/objects/hitbox.py
@@ -1,24 +1,23 @@
 import pyglet.sprite
 from pyglet import shapes, sprite
 
-
 class Hitbox:
-    def __init__(self, speed, size, x_pos, y_pos, background):
+    def __init__(self, speed, width, height, x_pos, y_pos, background, color):
         self.speed = speed
-        self.size = size
+        self.width = width
+        self.height = height
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.background = background
+        self.color = color
         # Png
         self.scale = 0.25
-        self.hitbox = shapes.Rectangle(200, 200, 100, 200, color=(55, 55, 255), batch=self.background)
-        self.cursor_sprite = sprite.Sprite(self.image, 0, 0)
-        self.image_width = self.image.width * self.scale
-        self.image_height = self.image.height * self.scale
+        self.hitbox = shapes.Rectangle(x_pos, y_pos, width, height, color=color, batch=self.background)
 
     def draw(self, x, y):
-        self.hitbox_sprite = pyglet.sprite.Sprite(self.image, x, y, batch=self.background)
-        self.hitbox_sprite.draw()
+        self.hitbox_rect = shapes.Rectangle(x, y, self.width, self.height, color=self.color, batch=self.background)
+        # self.hitbox_sprite = pyglet.sprite.Sprite(self.hitbox, x, y, batch=self.background)
+        self.hitbox_rect.draw()
 
     def update(self, dt):
         self.y_pos -= self.speed * dt
