29403a184f71616143c48190d17e2c88ad754b64
MrRobot420 <max.karlchen@web.de>
2021-04-15 22:26:32 +0200

    add Game class

--- src/game.py
+++ src/game.py
@@ -1,8 +1,12 @@
 import pyglet
 from gui.main_window import MainWindow
 
+class Game:
+    def __init__(self):
+        game_window = MainWindow(1280, 960, 'Game Name', resizable=True)
+        game_window.set_mouse_visible(False)  # Hide the mouse cursor
+        pyglet.clock.schedule_interval(game_window.update, 1/60.0)
+        pyglet.app.run()
+
 if __name__ == '__main__':
-    game_window = MainWindow(1280, 960, 'Game Name', resizable=True)
-    pyglet.clock.schedule_interval(game_window.update, 1/60.0)
-    game_window.set_mouse_visible(False)  # Hide the mouse cursor
-    pyglet.app.run()
+    Game()
