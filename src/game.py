6140594f77c8f0794aa533fbfde750129747a834
MrRobot420 <max.karlchen@web.de>
2021-04-15 23:24:30 +0200

    massive! refactoring of all components | add event_handler

--- src/game.py
+++ src/game.py
@@ -4,21 +4,22 @@ from gui.main_window import MainWindow
 from gui.elements.cursor import Cursor
 from gui.hud.hud import HUD
 from handler.enemy_handler import EnemyHandler
+from handler.game_event_handler import GameEventHandler
 
 class Game:
     def __init__(self):
-        background = pyglet.graphics.Batch()
-        width = 1280
-        height = 960
+        self.background = pyglet.graphics.Batch()
+        self.width = 1280
+        self.height = 960
         
-        cursor = Cursor(background)
-        enemy_handler = EnemyHandler(width, height, background) # Put current level in it.
-        hud = HUD(width, height, len(enemy_handler.enemies))
+        self.cursor = Cursor(self.background)
+        self.enemy_handler = EnemyHandler(self.width, self.height, self.background) # Put current level in it.
+        self.hud = HUD(self.width, self.height, len(self.enemy_handler.enemies))
 
-        game_window = MainWindow(cursor, enemy_handler, hud, width, height, 'Shoot Em’ Up!', resizable=True)
-        game_window.set_mouse_visible(False)  # Hide the mouse cursor
+        self.game_window = MainWindow(self.cursor, self.enemy_handler, self.hud, self.width, self.height, 'Shoot Em’ Up!', resizable=True)
+        self.game_window.set_mouse_visible(False)  # Hide the mouse cursor
 
-        pyglet.clock.schedule_interval(game_window.update, 1/60.0)
+        pyglet.clock.schedule_interval(self.game_window.update, 1/60.0)
         pyglet.app.run()
 
 if __name__ == '__main__':
--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,57 +1,32 @@
 import pyglet
 from pyglet.gl import *
 
-from gui.elements.cursor import Cursor
-from gui.hud.hud import HUD
-from handler.enemy_handler import EnemyHandler
-
-key = pyglet.window.key
-
+from handler.game_event_handler import GameEventHandler
 
 class MainWindow(pyglet.window.Window):
-    def __init__(self, cursor, enemy_handler, hud, *args, **kwrgs, ):
+    def __init__(self, cursor, enemy_handler, hud, *args, **kwrgs):
         super().__init__(*args, **kwrgs, vsync=False)
         pyglet.gl.glClearColor(0.9, 0.9, 0.9, 1)
-        self.x, self.y = 0, 0
-        
         self.cursor = cursor
         self.enemy_handler = enemy_handler
         self.hud = hud
 
-        self.keys = {}
         self.mouse_x = 0
         self.mouse_y = 0
         self.alive = 1
 
-    
-    def on_draw(self):
-        dt = pyglet.clock.tick()
-        self.render(dt)
-    
-
-    def on_close(self):
-        self.enemy_handler.enemies = []
-        self.alive = 0
+        self.game_event_handler = GameEventHandler(self.update_mouse_coordinates, self.alive)
+        self.push_handlers(self.game_event_handler)
 
 
-    def on_mouse_motion(self, x, y, dx, dy):
+    def update_mouse_coordinates(self, x, y):
         self.mouse_x = x
         self.mouse_y = y
 
 
-    def on_key_release(self, symbol, modifiers):
-        try:
-            del self.keys[symbol]
-        except:
-            pass
-
-
-    def on_key_press(self, symbol, modifiers):
-        if symbol == key.ESCAPE:  # [ESC]
-            self.enemy_handler.enemies = []
-            self.alive = 0
-
-        self.keys[symbol] = True
+    def on_draw(self):
+        dt = pyglet.clock.tick()
+        self.render(dt)
 
 
     def render(self, dt):
@@ -62,8 +37,9 @@ class MainWindow(pyglet.window.Window):
         self.cursor.draw(self.mouse_x, self.mouse_y)
         self.flip()
 
+
     def update(self, dt):
-        while self.alive == 1:
+        while self.game_event_handler.alive == 1:
             self.render(dt)
             self.dispatch_events()
         self.close()
